673c11344a201b55af2bfe387dca26c3fbb39f28
Andrea Pinardi <andreapinardi319@gmail.com>
2023-12-17 15:18:35 +0100

    TST: add test for convert_raw_mesh()

--- test_IO.py
+++ test_IO.py
@@ -16,6 +16,20 @@ def test_read_mesh_1D():
     assert success
 
 
+def test_raw_mesh_conversion():
+    """Test to convert raw mesh coordinates to .mesh input file"""
+    
+    raw_mesh_file = 'prova_raw_mesh.txt'
+    
+    io.convert_raw_mesh(raw_mesh_file, 'converted_mesh_test.mesh', 'meter')
+    mesh_converted = io.read_mesh('converted_mesh_test.mesh')
+    
+    mesh_exact = np.array([0. , 0.1, 0.2, 0.3, 0.4])
+    
+    success = np.array_equal(mesh_exact, mesh_converted)
+    
+    assert success
+    
 
 
 #test_read_mesh_1D()
