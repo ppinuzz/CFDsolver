c3e956c917f8c92f73e9c6d2cd8aaa8022f00538
Andrea Pinardi <andreapinardi319@gmail.com>
2023-12-18 21:41:21 +0100

    TST: add test for print_mesh()

--- test_pymesh.py
+++ test_pymesh.py
@@ -4,9 +4,9 @@ import numpy as np
 import pymesh as pmsh
 
 def test_read_mesh_1D():
-    """Test to read 1D .mesh file"""
+    """Test: read 1D .mesh file"""
     
-    mesh_file = 'prova.mesh'
+    mesh_file = 'test.mesh'
     
     mesh_exact = np.array([0. , 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1. ])
     mesh_read = pmsh.read_mesh(mesh_file)
@@ -17,19 +17,31 @@ def test_read_mesh_1D():
 
 
 def test_raw_mesh_conversion():
-    """Test to convert raw mesh coordinates to .mesh input file"""
+    """Test: convert raw mesh coordinates to .mesh input file"""
     
-    raw_mesh_file = 'prova_raw_mesh.txt'
+    raw_mesh_file = 'test_raw_mesh.txt'
+    converted_mesh_file = 'test_converted_mesh.mesh'
     
-    pmsh.convert_raw_mesh(raw_mesh_file, 'converted_mesh_test.mesh')
-    mesh_converted = pmsh.read_mesh('converted_mesh_test.mesh')
+    pmsh.convert_raw_mesh(raw_mesh_file, converted_mesh_file)
+    mesh_converted = pmsh.read_mesh(converted_mesh_file)
     
     mesh_exact = np.array([0. , 0.1, 0.2, 0.3, 0.4])
     
     success = np.array_equal(mesh_exact, mesh_converted)
     
     assert success
+
+
+def test_print_mesh():
+    """Test: print mesh to .mesh file"""
     
-
-
-#test_read_mesh_1D()
+    mesh = np.array([0, 0.1, 0.2, 0.3])
+    mesh_file = 'test_read_mesh.mesh'
+    pmsh.print_mesh(mesh, mesh_file)
+    
+    mesh_read = pmsh.read_mesh(mesh_file)
+    
+    success = np.array_equal(mesh, mesh_read)
+    
+    assert success
+    
